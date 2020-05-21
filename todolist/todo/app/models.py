1a673b9da357a6e6c1709514c44895b115d87192
seeke <seekeryang7@gmail.com>
2020-05-21 19:05:08 +0900

    todolist 과제

---
+++ todolist/todo/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True)
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
